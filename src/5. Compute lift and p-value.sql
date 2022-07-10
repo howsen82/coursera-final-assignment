d3dd27c59abc630185c4ebf8003862789f465aee
howsen82 <howsenpl@hotmail.com>
2022-07-10 13:48:53 +0800

    Add files via upload

--- src/5. Compute lift and p-value.sql
+++ src/5. Compute lift and p-value.sql
@@ -1,5 +1,46 @@
 --Use the https://thumbtack.github.io/abba/demo/abba.html to compute the lifts in metrics and the p-values for the binary metrics ( 30 day order binary and 30 day view binary) using a interval 95% confidence.
 
+SELECT
+test_assignment,
+COUNT(item_id) AS items,
+SUM(view_binary_30d) AS viewed_items
+FROM
+  (
+  SELECT 
+   fa.test_assignment,
+   fa.item_id, 
+   MAX(CASE WHEN views.event_time > fa.test_start_date THEN 1 ELSE 0 END)  AS view_binary_30d,
+   COUNT(views.event_id) AS views
+  FROM 
+    dsv1069.final_assignments fa
+    
+  LEFT OUTER JOIN 
+    (
+    SELECT 
+      event_time,
+      event_id,
+      CAST(parameter_value AS INT) AS item_id
+    FROM 
+      dsv1069.events 
+    WHERE 
+      event_name = 'view_item'
+    AND 
+      parameter_name = 'item_id'
+    ) views
+  ON 
+    fa.item_id = views.item_id
+  AND 
+    views.event_time >= fa.test_start_date
+  AND 
+    DATE_PART('day', views.event_time - fa.test_start_date ) <= 30
+  WHERE 
+    fa.test_number= 'item_test_2'
+  GROUP BY
+    fa.test_assignment,
+    fa.item_id
+) item_level
+GROUP BY test_assignment
+
 --For orders_bin: lift is -15% – 11% (-2.2%) and pval is 0.74
 --For views_bin: lift is and pval is -2.1% – 5.9% (1.9%) and pval is 0.36
 --Therefore for item_test_2, there was no significant difference in either the number of views or the number of orders between control and experiment
